fb4796de1769becd572b13954826c796b406227d
Andy Fragen <andy@thefragens.com>
2019-02-26 10:41:19 -0800

    update samples

--- samples/edd-sample-plugin/edd-sample-plugin.php
+++ samples/edd-sample-plugin/edd-sample-plugin.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: EDD Test Plugin
+Plugin Name: EDD Sample Plugin
 Description: Illustrates how to include an updater in your plugin for EDD Software Licensing
 Author: Andy Fragen
-Version: 0.9
+Version: 1.0
 License: MIT
 */
 
@@ -13,22 +13,11 @@ function edd_test_plugin_updater() {
 			'file'      => __FILE__,
 			'api_url'   => 'http://eddstore.test',
 			'item_name' => 'EDD Test Plugin',
-			'item_id'   => 11,       // ID of the product
-			'version'   => '0.9',                    // current version number
-			'author'    => 'Andy Fragen', // author of this plugin
+			'item_id'   => 11, // ID of the product.
+			'version'   => '1.0', // current version number.
+			'author'    => 'Andy Fragen', // author of this plugin.
 			'beta'      => false,
 		)
 	) )->load_hooks();
 }
 add_action( 'plugins_loaded', 'edd_test_plugin_updater' );
-add_filter(
-	'http_request_args',
-	function( $r, $url ) {
-		if ( false !== strpos( $url, 'eddstore.test' ) ) {
-			$r['reject_unsafe_urls'] = false;
-		}
-		return $r;
-	},
-	10,
-	2
-);

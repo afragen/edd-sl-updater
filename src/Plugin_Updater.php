635909b7e5df5fec8673e5fc9ebbef2de3241d19
Andy Fragen <andy@thefragens.com>
2019-02-27 20:05:13 -0800

    fix translator comment

--- src/Plugin_Updater.php
+++ src/Plugin_Updater.php
@@ -211,7 +211,7 @@ class Plugin_Updater {
 
 			if ( empty( $version_info->download_link ) ) {
 				printf(
-					/* translators: %1: item name, %2: open tag, %3: close tag, %4: version number */
+					/* translators: %1: item name, %2: open tag, %3: version number, %4: close tag */
 					__( 'There is a new version of %1$s available. %2$sView version %3$s details%4$s.', 'edd-sl-updater' ),
 					esc_html( $version_info->name ),
 					'<a target="_blank" class="thickbox" href="' . esc_url( $changelog_link ) . '">',

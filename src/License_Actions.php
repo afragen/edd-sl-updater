d9f75dce0a99e5fb54ade22d7df15fa439d8f779
Andy Fragen <andy@thefragens.com>
2019-05-02 10:44:48 -0700

    cleanup and create more specific error code and move outside of

--- src/License_Actions.php
+++ src/License_Actions.php
@@ -38,70 +38,73 @@ trait License_Actions {
 	}
 
 	/**
-	 * Activates the license.
+	 * Activate the license.
+	 *
+	 * Listen for our activate button to be clicked.
+	 * Exit early if we didn't click the Activate button.
 	 *
 	 * @since 1.0.0
 	 */
 	public function activate_license() {
-		// Listen for our activate button to be clicked.
-		if ( isset( $_POST[ $this->slug . '_license_activate' ] ) ) {
-			// run a quick security check.
-			if ( ! check_admin_referer( $this->slug . '_nonce', $this->slug . '_nonce' ) ) {
-				return; // Get out if we didn't click the Activate button.
-			}
-
-			// Data to send in our API request.
-			$api_params = [
-				'edd_action' => 'activate_license',
-				'license'    => $this->license,
-				'item_name'  => rawurlencode( $this->item_name ), // the name of our product in EDD.
-				'item_id'    => $this->item_id,
-				'url'        => home_url(),
-			];
-
-			add_filter( 'edd_sl_api_request_verify_ssl', '__return_false' );
-			$license_data = $this->get_api_response( $this->api_url, $api_params );
-
-			if ( ! $license_data->success && isset( $license_data->error ) ) {
-				switch ( $license_data->error ) {
-					case 'expired':
-						$message = sprintf(
-							$this->strings['license-key-expired-%s'],
-							date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
-						);
-						break;
-					case 'disabled':
-					case 'revoked':
-						$message = $this->strings['license-key-is-disabled'];
-						break;
-					case 'missing':
-						$message = $this->strings['status-invalid'];
-						break;
-					case 'invalid':
-					case 'site_inactive':
-						$this->strings['license-inactive-url'];
-						break;
-					case 'item_name_mismatch':
-						$message = sprintf( $this->strings['item-name-mismatch-%s'], $this->item_name );
-						break;
-					case 'no_activations_left':
-						$message = $this->strings['license-activation-limit'];
-						break;
-					default:
-						$message = $this->strings['error'];
-						break;
-				}
-			}
+		if ( ! isset( $_POST[ $this->slug . '_license_activate' ] ) ) {
+			return;
+		}
+		// run a quick security check.
+		if ( ! check_admin_referer( $this->slug . '_nonce', $this->slug . '_nonce' ) ) {
+			return;
 		}
 
+		// Data to send in our API request.
+		$api_params = [
+			'edd_action' => 'activate_license',
+			'license'    => $this->license,
+			'item_name'  => rawurlencode( $this->item_name ), // the name of our product in EDD.
+			'item_id'    => $this->item_id,
+			'url'        => home_url(),
+		];
+
+		add_filter( 'edd_sl_api_request_verify_ssl', '__return_false' );
+		$license_data = $this->get_api_response( $this->api_url, $api_params );
+
+		if ( ! $license_data->success && isset( $license_data->error ) ) {
+			switch ( $license_data->error ) {
+				case 'expired':
+					$message = sprintf(
+						$this->strings['license-key-expired-%s'],
+						date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
+					);
+					break;
+				case 'disabled':
+				case 'revoked':
+					$message = $this->strings['license-key-is-disabled'];
+					break;
+				case 'missing':
+					$message = $this->strings['status-invalid'];
+					break;
+				case 'invalid':
+				case 'site_inactive':
+					$this->strings['license-inactive-url'];
+					break;
+				case 'item_name_mismatch':
+					$message = sprintf( $this->strings['item-name-mismatch-%s'], $this->item_name );
+					break;
+				case 'no_activations_left':
+					$message = $this->strings['license-activation-limit'];
+					break;
+				default:
+					$message = $this->strings['error'];
+					break;
+			}
+		}
 		if ( isset( $license_data, $license_data->license ) ) {
 			update_option( $this->slug . '_license_key_status', $license_data->license );
 			delete_transient( $this->slug . '_license_message' );
 		}
-
 		if ( ! empty( $message ) ) {
-			$error_data['success']       = false;
-			$error_data['error_code']    = esc_attr__( 'activate_license', 'edd-sl-updater' );
+			$error_data['success']    = false;
+			$error_data['error_code'] =
+			/* translators: %s: item type 'plugin' or 'theme' */
+			sprintf( esc_attr__( 'activate_license_%s', 'edd-sl-updater' ), $this->data['type'] );
 			$error_data['error_message'] = esc_html( $message );
 		} else {
 			$error_data = null;
@@ -110,48 +113,52 @@ trait License_Actions {
 	}
 
 	/**
-	 * Deactivates the license.
+	 * Deactivate the license.
+	 *
+	 * Listen for our deactivate button to be clicked.
+	 * Exit early if we didn't click the deactivate button.
 	 *
 	 * @since 1.0.0
 	 */
 	public function deactivate_license() {
-		// Listen for our deactivate button to be clicked.
-		if ( isset( $_POST[ $this->slug . '_license_deactivate' ] ) ) {
-			// Run a quick security check.
-			if ( ! check_admin_referer( $this->slug . '_nonce', $this->slug . '_nonce' ) ) {
-				return; // get out if we didn't click the Deactivate button.
-			}
-
-			// data to send in our API request.
-			$api_params = [
-				'edd_action' => 'deactivate_license',
-				'license'    => $this->license,
-				'item_name'  => rawurlencode( $this->item_name ), // the name of our product in EDD.
-				'item_id'    => $this->item_id,
-				'url'        => home_url(),
-			];
-
-			// Call the custom API.
-			add_filter( 'edd_sl_api_request_verify_ssl', '__return_false' );
-			$license_data = $this->get_api_response( $this->api_url, $api_params );
-
-			// $license_data->license will be either "deactivated" or "failed".
-			if ( $license_data->success && property_exists( $license_data, 'error' ) ) {
-				$message = $this->strings['error'];
-			}
-			if ( ! empty( $message ) ) {
-				$error_data['success']       = false;
-				$error_data['error_code']    = esc_attr__( 'deactivate_license', 'edd-sl-updater' );
-				$error_data['error_message'] = esc_html( $message );
-			} else {
-				$error_data['success'] = true;
-			}
-
-			if ( ! isset( $license_data->license ) || 'deactivated' === $license_data->license ) {
-				delete_option( $this->slug . '_license_key_status' );
-				delete_transient( $this->slug . '_license_message' );
-			}
-			$this->redirect( $error_data );
+		if ( ! isset( $_POST[ $this->slug . '_license_deactivate' ] ) ) {
+			return;
 		}
+		// Run a quick security check.
+		if ( ! check_admin_referer( $this->slug . '_nonce', $this->slug . '_nonce' ) ) {
+			return;
+		}
+
+		// data to send in our API request.
+		$api_params = [
+			'edd_action' => 'deactivate_license',
+			'license'    => $this->license,
+			'item_name'  => rawurlencode( $this->item_name ), // the name of our product in EDD.
+			'item_id'    => $this->item_id,
+			'url'        => home_url(),
+		];
+
+		// Call the custom API.
+		add_filter( 'edd_sl_api_request_verify_ssl', '__return_false' );
+		$license_data = $this->get_api_response( $this->api_url, $api_params );
+
+		// $license_data->license will be either "deactivated" or "failed".
+		if ( $license_data->success && property_exists( $license_data, 'error' ) ) {
+			$message = $this->strings['error'];
+		}
+		if ( ! isset( $license_data->license ) || 'deactivated' === $license_data->license ) {
+			delete_option( $this->slug . '_license_key_status' );
+			delete_transient( $this->slug . '_license_message' );
+		}
+		if ( ! empty( $message ) ) {
+			$error_data['success']    = false;
+			$error_data['error_code'] =
+			/* translators: %s: item type 'plugin' or 'theme' */
+			sprintf( esc_attr__( 'activate_license_%s', 'edd-sl-updater' ), $this->data['type'] );
+			$error_data['error_message'] = esc_html( $message );
+		} else {
+			$error_data = null;
+		}
+		$this->redirect( $error_data );
 	}
 }
